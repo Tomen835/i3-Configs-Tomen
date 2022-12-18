6927d710b0bb9f482bcb61da64b21202d4f4f985
Tomen <109696984+Tomen835@users.noreply.github.com>
2022-12-18 17:17:38 +0000

    Creation of config store

---
+++ dotconfig/backup.sh
@@ -0,0 +1,6 @@
+cp ~/.config/alacritty/alacritty.yml ~/backups/alacritty
+cp ~/.config/picom/picom.conf ~/backups/picom
+cp ~/.config/rofi/* ~/backups/rofi
+cp ~/.config/i3/* ~/backups/i3
+cp ~/.config/polybar/* ~/backups/polybar
+
